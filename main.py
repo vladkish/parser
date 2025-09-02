ab4911334acb210fc41b7cece5837dfceebc29fa
vladkish <vladkish@users.noreply.github.com>
2025-09-03 01:26:53 +0300

    Working with bs4 and fnc find_all, find

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-# def main():
-#     import requests
-#     link = "https://www.codewars.com/dashboard"
-
-#     responce = requests.get(link)
-
-#     status = responce.status_code
-#     if status:
-#         with open('content.html', 'w', encoding='utf-8') as file:
-#             file.write(responce.text)
-#     else:
-#         return status
-
-# if __name__ == '__main__':
-#     if main() == None:
-#         print('Пошел наху')
-#     else:
-#         print(main())
-
 import requests
+from bs4 import BeautifulSoup
 
-link = "https://instagram.fplv1-2.fna.fbcdn.net/v/t51.2885-19/536972176_17860875546464588_1331139665925734216_n.jpg?stp=dst-jpg_s150x150_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4yMjUuYzIifQ&_nc_ht=instagram.fplv1-2.fna.fbcdn.net&_nc_cat=108&_nc_oc=Q6cZ2QHuVQxpVtK2i4Q0pXeamZ7hwdSBeSoWz4PJPgLPJJwc_kEzmXCfsF1UrbKhHHn_Njc&_nc_ohc=CnVvUXnefgYQ7kNvwGtNusI&_nc_gid=MofACTfe-FExqTMMdfNTbQ&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfXOklSxfiv0p8uOyijozWo1Ghd3K9f6AvSJYBc8NOntpg&oe=68BCA1AD&_nc_sid=8b3546"
-responce = requests.get(link, stream=True)
+link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
 
-with open('default.png', 'wb') as file:
-    for _ in responce.iter_content(chunk_size=8192):
-        file.write(_)
+responce = requests.get(link)
+
+with open('content.html', 'w', encoding='utf-8') as file:
+    file.write(responce.text)
+
+soup = BeautifulSoup(responce.text, 'lxml')
+
+context = {}
+
+block = soup.find('div', attrs={
+    'class' : 'bg-white rounded-2 p-4'
+})
+print(block)
