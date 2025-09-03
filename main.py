e176d6f95e46bdcd3af5518dd1fa92b9e653864b
vladkish <vladkish@users.noreply.github.com>
2025-09-03 13:52:52 +0300

    Complete idea test task

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
-
 responce = requests.get(link)
-status = responce.status_code
 
+context = {
+
+}
+
+# Ojbect html(page)
 soup = BeautifulSoup(responce.text, 'lxml')
 
-main_list = {}
+# Find main block
+block = soup.find_all('div', class_="br-pr-chr-item")[2]
 
-content = soup.find_all('div', class_="br-pr-chr-item")
+# Gpu block
+gpu = block.find('div')
 
-if __name__ == '__main__':
-    if status == 200:
-        try:
-            for _ in content:
-                main_list[_.text[1]] = _.text
-            with open('content.html', 'w', encoding='utf-8') as file:
-                file.write(str(main_list).replace('\n', ''))
-        except TypeError as e:
-            print("Dont't true agrs", e)
-    else:
-        print(status)
+gpu_list_for = str(gpu.text).replace(' ', '').split()
+
+for index, value in enumerate(gpu_list_for):
+    if index % 2 == 0:
+        context[value] = gpu_list_for[index+1]
+
+print(context)
