562d8e97846807b63304a7da482ff5ead3fd7dae
vladkish <vladkish@users.noreply.github.com>
2025-09-03 13:03:05 +0300

    Don't compelete test task

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from bs4 import BeautifulSoup
 link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
 
 responce = requests.get(link)
+status = responce.status_code
+
 soup = BeautifulSoup(responce.text, 'lxml')
 
-content = soup.find('div', class_="br-pr-chr")
-block = content.find('div', class_='br-pr-chr-item')
-information_block = block.find('div')
+main_list = {}
+
+content = soup.find_all('div', class_="br-pr-chr-item")
 
 if __name__ == '__main__':
-    if responce.status_code == 200:
+    if status == 200:
         try:
-            print(str(information_block.contents.replace(' ', '')))
-        except:
+            for _ in content:
+                main_list[_.text[1]] = _.text
             with open('content.html', 'w', encoding='utf-8') as file:
-                file.write(information_block.text.replace(' ', ''))
+                file.write(str(main_list).replace('\n', ''))
+        except TypeError as e:
+            print("Dont't true agrs", e)
     else:
-        print(responce.status_code)
+        print(status)
