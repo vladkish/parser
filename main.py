2a7051e181bdfead91fc63cb75dd3b66aae260cf
vladkish <vladkish@users.noreply.github.com>
2025-09-03 12:40:33 +0300

    Writing parser for mobile informations

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from bs4 import BeautifulSoup
 link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
 
 responce = requests.get(link)
-
-with open('content.html', 'w', encoding='utf-8') as file:
-    file.write(responce.text)
-
 soup = BeautifulSoup(responce.text, 'lxml')
 
-context = {}
+content = soup.find('div', class_="br-pr-chr")
+block = content.find('div', class_='br-pr-chr-item')
+information_block = block.find('div')
 
-block = soup.find('div', attrs={
-    'class' : 'bg-white rounded-2 p-4'
-})
-print(block)
+if __name__ == '__main__':
+    if responce.status_code == 200:
+        try:
+            print(str(information_block.contents.replace(' ', '')))
+        except:
+            with open('content.html', 'w', encoding='utf-8') as file:
+                file.write(information_block.text.replace(' ', ''))
+    else:
+        print(responce.status_code)
