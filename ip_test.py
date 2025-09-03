facff4f93494afb9d263b03a65697827a4f82e8a
vladkish <vladkish@users.noreply.github.com>
2025-09-03 14:48:51 +0300

    Modification fnc display_user_agent

--- ip_test.py
+++ ip_test.py
@@ -1,26 +1,27 @@
 import requests 
 from bs4 import BeautifulSoup
 
-link = 'https://www.whatismybrowser.com/?utm_source=chatgpt.com'
+'''
+Fnc - display realy user-agent script
+'''
 
-headers = {
-    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
-}
+def display_user_agent(headers):
+    link = 'https://www.whatismybrowser.com/?utm_source=chatgpt.com'
 
-responce = requests.get(link, headers=headers)
+    responce = requests.get(link, headers=headers)
 
-soup = BeautifulSoup(responce.text, 'lxml')
+    soup = BeautifulSoup(responce.text, 'lxml')
 
-# Fing ip
-block = soup.find('div', id = "readout-quinary")
+    # Fing ip
+    block = soup.find('div', id = "readout-quinary")
 
-if not block == None:
-    ip_from_a = block.find('a', class_="user_agent").text
-else:
-    print(None)
+    if not block == None:
+        ip_from_a = block.find('a', class_="user_agent").text
+    else:
+        return None
 
 
-try:
-    print(ip_from_a)
-except:
-    print(None)
+    try:
+        return ip_from_a
+    except:
+        return
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from ip_test import display_user_agent
+import fake_useragent
 
 link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
-responce = requests.get(link)
+user = fake_useragent.UserAgent().random
 
-context = {
-
+headers = {
+    'User-Agent' : user
 }
+
+responce = requests.get(link, headers=headers)
+
+context = {}
 
 # Ojbect html(page)
 soup = BeautifulSoup(responce.text, 'lxml')
@@ -31,3 +37,5 @@ if __name__ == '__main__':
             json.dump(context, file, indent=4, ensure_ascii=False)
     except:
         print("Don't save file")
+
+print(display_user_agent(headers))
