106543ebc5f93ab2a5972d1597c58bd0500f0c6a
vladkish <vladkish@users.noreply.github.com>
2025-09-05 01:05:09 +0300

    parsing user page and returning links to projects

--- scrapy/github.py
+++ scrapy/github.py
@@ -2,84 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import fake_useragent
 
-def avatar_github(username):
-    link = f"https://github.com/{username}"
+def github_projects(username):
 
-    user = fake_useragent.UserAgent().firefox
+    link = f'https://github.com/{username}?tab=repositories'
 
     headers = {
-        'user-agent' : user
+        'user-agent' : fake_useragent.UserAgent().random
     }
 
     responce = requests.get(link, headers=headers)
-    status = responce.status_code
 
-    if status == 200:
+    try:
 
-        try:
-            with open('../user_page.html', 'w', encoding='utf-8') as file:
-                file.write(responce.text)
-        except:
-            return "Don't save img"
+        context = {}
 
-        soup = BeautifulSoup(responce.text, 'lxml')
+        with open('../pages/project.html', 'w', encoding='utf-8') as file:
+            file.write(responce.text)
 
-        # BlOCK LINK FROM GITHUB
-        block = soup.find('div', class_="d-flex width-full position-relative")
-        try:
-            link_on_the_project = block.find('a', id = "853865861")
-
-            link = f'https://github.com{link_on_the_project.get('href')}'
-            responce = requests.get(link, headers=headers)
-
-            try:
-                with open('../first_project.html', 'w', encoding='utf-8') as file:
-                    file.write(responce.text)
-            except:
-                return "Don't save file"
-            
-            # Creating object
             soup = BeautifulSoup(responce.text, 'lxml')
-            
+            block = soup.find('div', id = "user-repositories-list")
+            block_ul = block.find_all('div', class_="d-inline-block mb-1")
+
             try:
-                block_text = soup.find('div', class_="Layout-sidebar").text
+                for content in block_ul:
+                    text = content.find('h3', class_='wb-break-all')
+                    link_on_the_project = text.find('a')
+                
+                    context[link_on_the_project.text.replace('\n', '').replace(' ', '')] = f'https://github.com{link_on_the_project.get('href')}'
+                return context
+            except:
+                return "Error"
 
-                # Filter block_text
-                clean_text = block_text.strip().replace("\n", "")
+    except:
+        if responce.text.lower() == "not found":
+            return "Don't have user"
 
-                try:
-                    with open('../content.html', 'w', encoding='utf-8') as file:
-                        file.write(clean_text)
-                except:
-                    return "Don't save img"
-
-            except AttributeError:
-                block_text = soup.find('div', class_="Layout-sidebar")
-
-            return clean_text
-        
-        except AttributeError as e:
-            return "Don't have files", e
-
-        block = soup.find('div', attrs={
-            'class' : 'position-relative d-inline-block col-2 col-md-12 mr-3 mr-md-0 flex-shrink-0'
-        })
-
-        link = block.find('a')
-
-        # Get url avatar
-        avatar = link.find('img', class_="avatar avatar-user width-full border color-bg-default").get('src')
-        responce_avatar = requests.get(avatar, headers=headers)
-
-        try:
-            with open('../avatar.png', 'wb') as file:
-                for _ in responce_avatar.iter_content(chunk_size=8192):
-                    if _:
-                        file.write(_)
-        except:
-            return "Don't save img"
-
-    else:
-        return status
-
-print(avatar_github('AlexeyTarasov77'))
+print(github_projects(input('enter username user from github - > ')))
