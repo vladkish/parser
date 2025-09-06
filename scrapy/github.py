82d5ff55a24f6e699426c0a926b47a3ef3a0d9d4
vladkish <vladkish@users.noreply.github.com>
2025-09-06 11:47:19 +0300

    creating login from requests and bs4

--- scrapy/github.py
+++ scrapy/github.py
@@ -2,39 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import fake_useragent
 
-def github_projects(username):
+def github_projects():
 
-    link = f'https://github.com/{username}?tab=repositories'
+    session = requests.Session()
+
+    link = 'https://streetwear.com.ua/user/login'
+
+    data = {
+        'LoginForm[login]' : 'kishkovarv@gmail.com',
+        'LoginForm[password]' : 'Kish2008V'
+    }
 
     headers = {
         'user-agent' : fake_useragent.UserAgent().random
     }
+    
+    responce = session.post(link, headers=headers, data=data)
 
-    responce = requests.get(link, headers=headers)
-
-    try:
-
-        context = {}
-
-        with open('../pages/project.html', 'w', encoding='utf-8') as file:
-            file.write(responce.text)
-
-            soup = BeautifulSoup(responce.text, 'lxml')
-            block = soup.find('div', id = "user-repositories-list")
-            block_ul = block.find_all('div', class_="d-inline-block mb-1")
-
-            try:
-                for content in block_ul:
-                    text = content.find('h3', class_='wb-break-all')
-                    link_on_the_project = text.find('a')
-                
-                    context[link_on_the_project.text.replace('\n', '').replace(' ', '')] = f'https://github.com{link_on_the_project.get('href')}'
-                return context
-            except:
-                return "Error"
-
-    except:
-        if responce.text.lower() == "not found":
-            return "Don't have user"
-
-print(github_projects(input('enter username user from github - > ')))
+    if responce.status_code == 200:
+        return responce.text
+    else:
+        return responce.status_code
+    
+print(github_projects())
