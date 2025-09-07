acc0445313869ecee0b4837b52d8fccba9e2f62b
vladkish <vladkish@users.noreply.github.com>
2025-09-07 05:16:57 +0300

    End lesson five

--- scrapy/main.py
+++ scrapy/main.py
@@ -5,44 +5,46 @@ import fake_useragent
 import shutil
 import os
 
+number_page = 15
 image_number = 1
-storage_page = 1
-for storage in range(50):
+for page in range(2):
 
-    link = f"https://books.toscrape.com/catalogue/page-{storage_page}.html"
-    
+    link = f'https://books.toscrape.com/catalogue/page-{number_page}.html'
+
     responce = requests.get(link)
-
     soup = BeautifulSoup(responce.text, 'lxml')
 
-    main_block = soup.find('div', class_="col-sm-8 col-md-9")
-    section = main_block.find('section')
-    div_row = section.find_all('div')[1]
-    ol = div_row.find('ol', class_="row")
+    block_main = soup.find('div', class_='col-sm-8 col-md-9')
+    section = block_main.find('section')
+    two_div = section.find_all('div')[1]
+    li_all = two_div.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
-    # All books from first page
-    images = ol.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-
-    for image in images:
-        block = image.find('div', class_="image_container")
+    for li in li_all:
+        product_pod = li.find('article', class_="product_pod")
+        block = product_pod.find('div', class_="image_container")
         a_link = block.find('a').get('href')
-        donwload_image = f'https://books.toscrape.com/catalogue/{a_link}'
 
-        # responce
-        page = requests.get(donwload_image)
-        donwload = BeautifulSoup(page.text, 'lxml')
+        total_link = f'https://books.toscrape.com/catalogue/{a_link}'
+        responce = requests.get(total_link)
 
-        main_block = donwload.find('div', class_="col-sm-6")
-        carousel_inner = main_block.find('div', class_="carousel-inner")
-        item_active = carousel_inner.find('div', class_="item active")
-        img = item_active.find('img').get('src')
+        soup_result = BeautifulSoup(responce.text, 'lxml')
 
-        link_download = f'https://books.toscrape.com/{img[6:]}'
-        bytes_image = requests.get(link_download).content
+        with open('../pages/books.html', 'w', encoding='utf-8') as file:
+            file.write(soup_result.text)
+
+        # Find img url
+        product_gallery = soup_result.find('div', id="product_gallery")
+
+        thumbnail = product_gallery.find('div', class_="thumbnail")
+        block = thumbnail.find('div', class_="carousel-inner")
+        block_img = block.find('div', class_="item active")
         
+        download_img = block_img.find('img').get('src')
+        total_download = f'https://books.toscrape.com/{download_img[6:]}'
+        
+        img_reposnce = requests.get(total_download).content
+
         with open(f'../img/{image_number}.jpg', 'wb') as file:
-            file.write(bytes_image)
-            image_number += 1
-        
-        print('img donwload', image_number)
-    storage_page += 1
+            file.write(img_reposnce)
+        image_number += 1
+        number_page += 1
