9092711b4e7a78cfddadd93ddbc6aaadfe5f8a9b
vladkish <vladkish@users.noreply.github.com>
2025-09-07 03:12:51 +0300

    Display all link from first page

--- scrapy/main.py
+++ scrapy/main.py
@@ -2,39 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import fake_useragent
+import shutil
+import os
 
-link = 'https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html'
+storage_page = 0
 
-# Setting headers
-user = fake_useragent.UserAgent().random
+link = "https://books.toscrape.com/catalogue/page-1.html"
+ 
+responce = requests.get(link)
 
-headers = {
-    'User-Agent' : user
-}
-
-responce = requests.get(link, headers=headers)
-
-context = {}
-
-# Ojbect html(page)
 soup = BeautifulSoup(responce.text, 'lxml')
 
-# Find main block
-block = soup.find_all('div', class_="br-pr-chr-item")
+main_block = soup.find('div', class_="col-sm-8 col-md-9")
+section = main_block.find('section')
+div_row = section.find_all('div')[1]
+ol = div_row.find('ol', class_="row")
 
-for content in block:
-    # Gpu block
-    gpu = content.find('div')
+images = ol.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 
-    gpu_list_for = str(gpu.text).replace(' ', '').split()
-
-    for index, value in enumerate(gpu_list_for):
-        if index % 2 == 0:
-            context[value] = gpu_list_for[index+1]
-
-if __name__ == '__main__':
-    try:
-        with open('../main.json', 'w', encoding='utf-8') as file:
-            json.dump(context, file, indent=4, ensure_ascii=False)
-    except:
-        print("Don't save file")
+for image in images:
+    block = image.find('div', class_="image_container")
+    a_link = block.find('a').get('href')
+    print(f'https://books.toscrape.com/catalogue/{a_link}')
