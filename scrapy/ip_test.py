f94089047e2dc9d1260f9577ad4e1ccf0b736baa
vladkish <vladkish@users.noreply.github.com>
2025-09-07 06:09:00 +0300

    Practice work with session

--- scrapy/ip_test.py
+++
@@ -1,4 +0,0 @@
-def array_diff(a, b):
-    for index, value in enumerate(b):
-        if value in a:
-            pass
---
+++ scrapy/test_first.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+
+link = 'https://brain.com.ua/api/v1/login'
+
+session1 = requests.Session()
+
+headers = {
+    'user-agent' : fake_useragent.UserAgent().random
+}
+
+data = {
+    'phone_number' : '+38 (096) 14 17 176',
+    'password' : '417267'
+}
+
+responce = session1.post(link, headers=headers, data=data)
+
+cookies_dict = []
+for cookie in session1.cookies:
+    cookies_info = {
+        'domain' : cookie.domain,
+        'name' : cookie.name,
+        'path' : cookie.path, 
+        'value' : cookie.value
+    }
+    cookies_dict.append(cookies_info)
+
+# Session2
+session2 = requests.Session()
+
+for cookie in cookies_dict:
+    session2.cookies.set(**cookie)
+
+link = 'https://brain.com.ua/ukr/cabinet/'
+
+responce = session2.get(link)
+
+for cookie in session2.cookies:
+    print(cookie)
+
+# Ready for work
